c3aa152fca0d59413826c9533a882a56c73bc5f7
lon91ong <lon91ong@gmail.com>
2019-12-02 11:47:01 +0800

    Delete popup.css

---
+++ README.rst
@@ -0,0 +1,39 @@
+自动填写成绩
+===============
+
+使用说明
+----------
+
+SELECT id, score FROM students WHERE class = 'Your Class'
+
+如上所述，成绩保存在students表单里，students表单有学号（id）、姓名（name）、班级（class）和成绩（score）四个字段
+（待续...）
+
+JavaScript的习作
+------------------
+
+用时一周，代码不多，关键是不习惯JavaScript太灵活的语法，慢慢啃，好歹是东拼西凑的整出来了！
+
+还有待学习的几个方面：
+
+- Promise 这个和平成接触的python用法很不一样，虽然懵懵懂懂的用对了一下，但是还没整太明白，需要再学学。
+ 参考资料 `迷你书 <http://liubin.org/promises-book/>`_ ; ES6 Promise 简明指南:`中文版 <https://ifyour.github.io/2018/05/29/ES6-Promise-简明指南/>`_ , `英文版 <https://codeburst.io/a-simple-guide-to-es6-promises-d71bacd2e13a>`_ ; `简明实例 <https://blog.dingkewz.com/post/tech/javascript_promise/>`_
+
+- MutationObserver 这个监听没整明白
+ 参考 `高效监听DOM变动 <https://hijiangtao.github.io/2017/08/03/How-to-Manipulate-DOM-Effectively/>`_ `参考二 <https://javascript.ruanyifeng.com/dom/mutationobserver.html>`_
+
+记忆深刻的坑
+--------------
+
+**iframe里面的子页面元素提取，空耗了半天的时间在Google上转悠，真真的巨坑啊！**
+
+autofill.js文件里面大多数注释掉的代码都是在这个坑里转悠的记录
+
+插件开发的高价值资料
+----------------------
+
+- `【干货】Chrome插件(扩展)开发全攻略 <http://www.cnblogs.com/liuxianan/p/chrome-plugin-develop.html>`_
+   个人觉得比 `360插件开发文档 <http://open.chrome.360.cn/extension_dev/overview.html>`_ 实用的多
+- `火狐的官方文档 <https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/Port>`_
+   有 `中文的 <https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/A_re-introduction_to_JavaScript>`_ ，但是翻译的不全
+
---
+++ scsvr_falcon.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 18:58:34 2019
+
+@author: Administrator
+"""
+
+from sqlite3 import connect#, OperationalError
+from time import localtime
+from os import path #, system
+from sys import exit as sysexit
+from myMod import mbox
+from urllib.parse import unquote
+
+import falcon
+from json import dumps,loads
+from waitress import serve
+
+from falcon.http_status import HTTPStatus
+
+class HandleCORS(object):
+    def process_request(self, req, resp):
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Methods', '*')
+        resp.set_header('Access-Control-Allow-Headers', '*')
+        resp.set_header('Access-Control-Max-Age', 1728000)  # 20 days
+        if req.method == 'OPTIONS':
+            raise HTTPStatus(falcon.HTTP_200, body='\n')
+
+class Resource(object):
+    def __init__(self, dbpth, act):
+        self.dbpth = dbpth
+        self.act = act
+    def dict_factory(self, cursor, row):
+            return dict((col[0], row[idx]) for idx, col in enumerate(cursor.description))
+    def on_get(self, req, resp):
+        print(req.url)
+        conn = connect(self.dbpth)
+        #print('Query Cmd:', unquote(req.url))
+        if self.act == 'classes':
+            curs = conn.cursor()
+            curs.execute('SELECT class FROM classes')
+            resp.body = ','.join(t[0] for t in map(list,curs.fetchall()))
+        elif self.act=='class':
+            if req.headers["CLIENT"]=='Excel':
+                conn.row_factory = self.dict_factory
+                curs = conn.cursor()
+                curs.execute('SELECT id,name,score FROM students where class ="'+unquote(req.query_string)+'"')
+                resp.body = dumps(curs.fetchall(),ensure_ascii=False)
+            else: # Greasemonkdy Script get scores to fill Web Table
+                curs = conn.cursor()
+                curs.execute('SELECT id,score FROM students where class ="'+unquote(req.query_string)+'"')
+                resp.body = dumps(dict(curs.fetchall()),ensure_ascii=False)
+        else:
+            resp.body = '非法途径访问！'
+            #resp.body = dumps({"success":False,"Contents":"Bad Request Command!"})
+        resp.status = falcon.HTTP_200
+        conn.close()
+    def on_post(self, req, resp):
+        print(req.url)
+        conn = connect(self.dbpth)
+        curs = conn.cursor()
+        #scores = req.media.encode('utf-8')
+        #print(scores)
+        if req.headers["CLIENT"]=='Excel':
+            scores = loads(req.media)
+            for score in scores:
+                curs.execute('UPDATE students SET score = '+str(score["score"])+' WHERE ID = "'+str(score["id"])+'"')
+            print('更新{}条记录！'.format(len(scores)))
+            resp.body = dumps({"success": True,"Content":'更新{}条记录！'.format(len(scores))},ensure_ascii=False)
+        else:
+            resp.body = '非法途径访问！'
+        resp.status = falcon.HTTP_201
+        conn.close()
+
+dbpath = path.dirname(path.realpath(__file__)) + "/hwmy"
+if localtime().tm_mon < 8:
+    dbpath += str(localtime().tm_year-2)[-2:]+".db"
+else:
+    dbpath += str(localtime().tm_year-1)[-2:]+".db"
+if not path.isfile(dbpath):
+    print('No database file in exe path, check please!')
+    mbox('错误','未能在程序目录下找到数据库文件{}！\n请查验后重试！'.format(dbpath[2:]),'error')
+    sysexit(0)
+    
+app = falcon.API(middleware=[HandleCORS()])
+
+app.add_route('/classes', Resource(dbpath,'classes'))
+app.add_route('/class', Resource(dbpath,'class'))
+app.add_route('/updata', Resource(dbpath,'updata'))
+
+serve(app, listen='*:8001')
+
+
+
+
