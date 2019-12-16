31f48b5ab828bf64ee486b9bc423517e2f942c33
lon91ong <lon91ong@gmail.com>
2019-12-16 14:05:35 +0800

    修正了提交成绩无法存入数据库的bug

--- scsvr_falcon.py
+++ scsvr_falcon.py
@@ -22,12 +22,14 @@ if localtime().tm_mon < 8 and localtime().tm_mon > 1: # 春季学期
     dbpath += str(localtime().tm_year-2)[-2:]+"_spring.db"
 else: #秋季学期
     dbpath += str(localtime().tm_year-1)[-2:]+"_autumn.db"
+
 if not path.isfile(dbpath):
     print('No database file in exe path, check please!')
     mbox('错误','未能在程序目录下找到数据库文件{}！\n请查验后重试！'.format(dbpath[2:]),'error')
     sysexit(0)
+print('Database File:',dbpath)
 
-def dict_factory(cursor, row):
+def dict_factory(self, cursor, row):
     return dict((col[0], row[idx]) for idx, col in enumerate(cursor.description))
 
 class HandleCORS(object):
@@ -40,29 +42,36 @@ class HandleCORS(object):
             raise HTTPStatus(falcon.HTTP_200, body='\n')
 
 class Resource(object):
-    global dapth,dict_factory
+    global dbpath,dict_factory
+
     def on_get(self, req, resp):
         print("URL:", req.url)
         conn = connect(dbpath)
-        #print('Query Cmd:', unquote(req.url))
         if req.path == '/classes':
             curs = conn.cursor()
             curs.execute('SELECT class FROM classes')
             resp.body = ','.join(t[0] for t in map(list,curs.fetchall()))
         elif req.path =='/class':
-            if req.headers["CLIENT"]=='Excel':
-                conn.row_factory = dict_factory
-                curs = conn.cursor()
-                curs.execute('SELECT id,name,score FROM students where class ="'+unquote(req.query_string)+'"')
-                resp.body = dumps(curs.fetchall(),ensure_ascii=False)
-            else: # Greasemonkdy Script get scores to fill Web Table
-                curs = conn.cursor()
-                curs.execute('SELECT id,score FROM students where class ="'+unquote(req.query_string)+'"')
-                resp.body = dumps(dict(curs.fetchall()),ensure_ascii=False)
+            cla = unquote(req.query_string)
+            try:
+                print("CLIENT:",req.headers["CLIENT"],"\tClass:",cla)
+                if req.headers["CLIENT"]=='Greasemonkey': # Get scores to fill Web Table
+                    curs = conn.cursor()
+                    curs.execute('SELECT id,score FROM students WHERE class ="'+cla+'"')
+                    resp.body = dumps(dict(curs.fetchall()),ensure_ascii=False)
+                else: # Excel
+                    conn.row_factory = dict_factory
+                    curs = conn.cursor()
+                    curs.execute('SELECT id,name,score FROM students WHERE class ="'+cla+'"')
+                    resp.body = dumps(curs.fetchall(),ensure_ascii=False)
+            except:
+                resp.body = '非法途径访问！'
+                pass
         else:
             resp.body = '非法途径访问！'
             #resp.body = dumps({"success":False,"Contents":"Bad Request Command!"})
         resp.status = falcon.HTTP_200
+        curs.close()
         conn.close()
     def on_post(self, req, resp):
         print("URL:", req.url)
@@ -71,19 +80,22 @@ class Resource(object):
         if req.path == '/updata' and req.headers["CLIENT"]=='Excel':
             scores = loads(req.media)
             for score in scores:
-                curs.execute('UPDATE students SET score = '+str(score["score"])+' WHERE ID = "'+str(score["id"])+'"')
+                curs.execute('UPDATE students SET score = '+str(score["score"])+' WHERE ID = '+str(score["id"]))
+            conn.commit()
             print('更新{}条记录！'.format(len(scores)))
             resp.body = dumps({"success": True,"Content":'更新{}条记录！'.format(len(scores))},ensure_ascii=False)
         else:
             resp.body = '非法途径访问！'
         resp.status = falcon.HTTP_201
+        curs.close()
         conn.close()
+        
+svrRes = Resource()
 
 app = falcon.API(middleware=[HandleCORS()])
 
-app.add_route('/classes', Resource())
-app.add_route('/class', Resource())
-app.add_route('/updata', Resource())
+app.add_route('/classes', svrRes)
+app.add_route('/class', svrRes)
+app.add_route('/updata', svrRes)
 
 serve(app, listen='*:8001')
-
